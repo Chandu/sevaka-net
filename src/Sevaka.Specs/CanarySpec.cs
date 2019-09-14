f9618abe775b51b0e10c0f7632986e15d3688ebd
Sirisha <sirisha.anupindi@GMail.com>
2019-09-14 12:24:29 +0530

    Added Specs Project

---
+++ src/Sevaka.Specs/CanarySpec.cs
@@ -0,0 +1,22 @@
+﻿using Machine.Specifications;
+
+namespace Sevaka.Specs
+{
+  [Subject("CanarySpec")]
+  internal class When_adding_two_numbers
+  {
+    private static int first, second;
+    private static int result;
+    private static int expected = 3;
+
+    private Establish context = () =>
+     {
+       first = 1;
+       second = 2;
+     };
+
+    private Because of = () => result = first + second;
+
+    private It should_have_valid_sum_value = () => result.ShouldEqual(expected);
+  }
+}
---
+++ src/Sevaka/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sevaka
+{
+  internal class Program
+  {
+    private static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
